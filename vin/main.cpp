460409772e51245813b2dff806a137f521c406ef
Goran <goranvel@gmail.com>
2022-10-02 06:58:52 -0500

    CMake improvements

--- vin/main.cpp
+++ vin/main.cpp
@@ -3,10 +3,15 @@
 #include "vin.h"
 
 int main (int argc, char** argv) {
-	std::string vinNo = argv[1];
+	std::string vinNo = "3A3";
+	if (argc == 2)
+		vinNo = argv[1];
+	else if (argc >= 2)
+		return -1;
 
 	Vin vin (vinNo);
 	std::cout << vin.getCountry ();
 	// std::cout << vin.getMake () << "\n";
+	std::cout << "\n";
 	return 0;
 }
